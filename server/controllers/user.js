a2425639c2a1cad5b6d83d4b97d5f229ece24284
Shreedhar Kumar Kushwaha <shreedharkushwaha7@gmail.com>
2022-08-29 22:13:16 +0530

    compleated users API section and run successfully upto this point

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,8 +4,22 @@ import makeValidation from "@withvoid/make-validation";
 import UserModel, { USER_TYPES } from "../models/User.js";
 
 export default {
-  onGetAllUsers: async (req, res) => {},
-  onGetUserById: async (req, res) => {},
+  onGetAllUsers: async (req, res) => {
+    try {
+      const users = await UserModel.getUsers();
+      return res.status(200).json({ success: true, users });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
+  onGetUserById: async (req, res) => {
+    try {
+      const user = await UserModel.getUserById(req.params.id);
+      return res.status(200).json({ success: true, user });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
 
   onCreateUser: async (req, res) => {
     try {
@@ -27,5 +41,15 @@ export default {
     }
   },
 
-  onDeleteUserById: async (req, res) => {},
+  onDeleteUserById: async (req, res) => {
+    try {
+      const user = await UserModel.deleteByUserById(req.params.id);
+      return res.status(200).json({
+        success: true,
+        message: `Deleted a count of ${user.deletedCount} user.`,
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
 };
